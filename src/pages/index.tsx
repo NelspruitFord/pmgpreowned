469e71328a1612ecbfbee81b8b64c6361bb968e2
Kewan Seymour <kewan3272001@gmail.com>
2023-07-28 16:02:06 +0200

    Trigger redeploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         <div>
           <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-              Put cars here
+              {/* Map cars here */}
             </div>
           </div>
         </div>
